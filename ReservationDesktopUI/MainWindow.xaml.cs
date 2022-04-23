b7c8e7d0a799b2a2378108e9aa09f0056bf7e01c
xkudel13 <finen@centrum.cz>
2022-04-23 13:29:28 +0200

    GUI

---
+++ ReservationDesktopUI/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReservationDesktopUI
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "The CheckBox is checked.";
+        }
+
+        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "The CheckBox is unchecked.";
+        }
+
+        private void HandleThirdState(object sender, RoutedEventArgs e)
+        {
+            text1.Text = "The CheckBox is in the indeterminate state.";
+        }
+
+        private void OpenPage(object sender, RoutedEventArgs e)
+        {
+            Zkouska obj = new Zkouska();
+            //this.Visibility = Visibility.Hidden;
+
+        }
+    }
+}
